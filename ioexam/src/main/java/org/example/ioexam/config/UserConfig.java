8d47d29fec2a564765e476f7cb1ca374f83ada2d
hanseu9839 <hanseu9839@gmail.com>
2024-05-13 10:23:28 +0900

    컴포넌트

--- ioexam/src/main/java/org/example/ioexam/config/UserConfig.java
+++ ioexam/src/main/java/org/example/ioexam/config/UserConfig.java
@@ -8,26 +8,27 @@ import org.example.ioexam.service.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan(basePackages="org..example.ioexam")
+@ComponentScan(basePackages="org..example.ioexam")
 public class UserConfig {
     // IOC 컨테이너에 Bean을 동락하는 방법을 생각해보고 동작 할 수 있도록 작성해주세요.
     // 1. JavaConfig를 이용해서 동작하도록!!
 
-    @Bean
-    public UserDao userDao(){
-        return new UserDaoImpl();
-    }
-
-    @Bean
-    public UserService userService(UserDao userDao){
-        return new UserServiceImpl(userDao);
-    }
-
-    @Bean
-    public UserController userController(UserServiceImpl userService){
-        return new UserController(userService);
-    }
+//    @Bean
+//    public UserDao userDao(){
+//        return new UserDaoImpl();
+//    }
+//
+//    @Bean
+//    public UserService userService(UserDao userDao){
+//        return new UserServiceImpl(userDao);
+//    }
+//
+//    @Bean
+//    public UserController userController(UserServiceImpl userService){
+//        return new UserController(userService);
+//    }
 
     // 2. component scan 동작되도록 살펴보세요..
-
+    // 2-1 누가 컴포넌트인지 알려줘야함.
+    // 2-2 componentScan을 추가한다.
 }
