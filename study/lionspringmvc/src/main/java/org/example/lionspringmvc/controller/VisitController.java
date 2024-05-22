9ddefdd9a7d71794bcca6079480065a84a3efc79
hanseu9839 <hanseu9839@gmail.com>
2024-05-22 11:25:24 +0900

    Spring Session

--- study/lionspringmvc/src/main/java/org/example/lionspringmvc/controller/VisitController.java
+++ study/lionspringmvc/src/main/java/org/example/lionspringmvc/controller/VisitController.java
@@ -1,6 +1,7 @@
 package org.example.lionspringmvc.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,16 +11,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VisitController {
 
-    @GetMapping("/visit")
+    @GetMapping("/cookie")
     public String showVisit(
             @CookieValue(name = "lastVisit", defaultValue="N/A") String lastVisit,
-            HttpServletResponse response, Model model
+            @CookieValue(name = "userId", defaultValue="") String userId,
+            HttpServletResponse response, Model model, HttpServletRequest request
     ){
         Cookie cookie = new Cookie("lastVisit", "carami");
         cookie.setMaxAge(2*60*60);
         response.addCookie(cookie);
 
+        Cookie cookie2 = new Cookie("userId", "kim");
+        cookie2.setMaxAge(60*60);
+        response.addCookie(cookie2);
+
         model.addAttribute("lastVisit", lastVisit);
+
+        Cookie[] cookies = request.getCookies();
+        for(Cookie c : cookies){
+            System.out.println(c.getName() + " ::: ");
+            System.out.println(c.getValue());
+        }
         return "visit";
     }
 }
---
+++ study/login/src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,20 @@
+package hello.login.web.member;
+
+
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute(member))
+}
